4e91426992cbc6fe38a6123f3d98ca8730dbe388
SANTIAGOE17 <chafitosantiago10@gmail.com>
2020-10-29 00:28:26 -0500

    COMENTARIO DE VARIABLE EN MainPage.xaml.cs

--- SEMANAS3_SANTIAGOE/MainPage.xaml.cs
+++ SEMANAS3_SANTIAGOE/MainPage.xaml.cs
@@ -18,9 +18,10 @@ namespace SEMANAS3_SANTIAGOE
         }
         private async void btnVentana_Clicked(object sender, EventArgs e)
         {
+            // DEFINICON DE VARIABLE
             string user = txtUser.Text;
             string pass = txtPass.Text;
-
+            
             if (user.Equals("estudiante2020") && pass.Equals("uisrael2020"))
             {
                 await Navigation.PushAsync(new vista2(user, pass));
